d3776ec6bf98238d410040803597e05b8f22b189
JuhoP <juzeliuz@suomi24.fi>
2019-01-26 00:14:44 +0200

    Created object placement scripts. Added fps counter.

---
+++ FGJ19/Assets/Code/FPSDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FPSDisplay : MonoBehaviour {
+
+    private const float updateInterval = 0.3f;
+    private const string prefixText = "FPS: ";
+
+    private TMP_Text text;
+    private float lastUpdateTime;
+    private List<float> frameTimes;
+
+    void Start() {
+        text = GetComponent<TMP_Text>();
+        lastUpdateTime = Time.time;
+        frameTimes = new List<float>();
+        text.text = prefixText + (1 / Time.deltaTime).ToString("F0");
+    }
+
+    void Update() {
+        frameTimes.Add(Time.deltaTime);
+
+        if(Time.time - lastUpdateTime >= updateInterval) {
+            lastUpdateTime = Time.time;
+            var totalFrameTime = 0f;
+            foreach(var ft in frameTimes) {
+                totalFrameTime += ft;
+            }
+
+            var intervalAverageFPS = 1 / (totalFrameTime / frameTimes.Count);
+            text.text = prefixText + intervalAverageFPS.ToString("F0");
+            frameTimes = new List<float>();
+        }
+    }
+}
---
+++ FGJ19/Assets/Code/PoissonDiscSampling.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PoissonDiscSampling {
+
+    //TODO: 
+    //Placement density fading towards edges 
+    //  (= increase radius when further away from center point?)
+
+    public static List<Vector2> GeneratePoints(float radius, float minDistanceFromCenter,
+        float maxDistanceFromCenter, int numSamplesBeforeRejection = 30) {
+
+        float sampleRegionSize = maxDistanceFromCenter * 2 + 1;
+        float cellSize = radius / Mathf.Sqrt(2);
+
+        int[,] grid = new int[Mathf.CeilToInt(sampleRegionSize / cellSize),
+            Mathf.CeilToInt(sampleRegionSize / cellSize)];
+        List<Vector2> points = new List<Vector2>();
+        List<Vector2> spawnPoints = new List<Vector2>();
+        float sqrMinDistanceFromCenter = minDistanceFromCenter * minDistanceFromCenter;
+        float sqrMaxDistanceFromCenter = maxDistanceFromCenter * maxDistanceFromCenter;   
+        Vector2 areaCenter = Vector2.one * sampleRegionSize / 2;
+
+        spawnPoints.Add(areaCenter + Vector2.up * minDistanceFromCenter);// * 1.5f);
+        while(spawnPoints.Count > 0) {
+            int spawnIndex = Random.Range(0, spawnPoints.Count);
+            Vector2 spawnCenter = spawnPoints[spawnIndex];
+            bool candidateAccepted = false;
+
+            for(int i = 0; i < numSamplesBeforeRejection; i++) {
+                float angle = Random.value * Mathf.PI * 2;
+                Vector2 dir = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle));
+                Vector2 candidate = spawnCenter + dir * Random.Range(radius, 2 * radius);
+
+                if(IsValid(candidate, sampleRegionSize, areaCenter, sqrMinDistanceFromCenter,
+                    sqrMaxDistanceFromCenter, cellSize, radius, points, grid)) {
+                    spawnPoints.Add(candidate);
+                    
+                    points.Add(candidate);
+                    grid[(int)(candidate.x / cellSize), (int)(candidate.y / cellSize)]
+                        = points.Count;
+                    candidateAccepted = true;
+                    break;
+                }
+            }
+
+            if(!candidateAccepted) {
+                spawnPoints.RemoveAt(spawnIndex);
+            }
+        }
+
+        return points;
+    }
+
+    private static bool IsValid(Vector2 candidate, float sampleRegionRadius, Vector2 areaCenter,
+        float sqrMinDistanceFromCenter, float sqrMaxDistanceFromCenter, float cellSize,
+        float radius, List<Vector2> points, int[,] grid) {
+
+        if(candidate.x >= 0 && candidate.x < sampleRegionRadius
+            && candidate.y >= 0 && candidate.y < sampleRegionRadius) {
+            
+            float sqrDstToCenter = (candidate - areaCenter).sqrMagnitude;
+            if(sqrDstToCenter < sqrMinDistanceFromCenter
+                || sqrDstToCenter > sqrMaxDistanceFromCenter) {
+                return false;
+            }
+
+            int cellX = (int)(candidate.x / cellSize);
+            int cellY = (int)(candidate.y / cellSize);
+
+            int searchStartX = Mathf.Max(0, cellX - 2);
+            int searchEndX = Mathf.Min(cellX + 2, grid.GetLength(0) - 1);
+            int searchStartY = Mathf.Max(0, cellY - 2);
+            int searchEndY = Mathf.Min(cellY + 2, grid.GetLength(1) - 1);
+
+            for(int x = searchStartX; x <= searchEndX; x++) {
+                for(int y = 0; y <= searchEndY; y++) {
+                    int pointIndex = grid[x, y] - 1;
+                    if(pointIndex != -1) {
+                        float sqrDstToOtherPoint = (candidate - points[pointIndex]).sqrMagnitude;
+                        if(sqrDstToOtherPoint < radius * radius) {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+}
