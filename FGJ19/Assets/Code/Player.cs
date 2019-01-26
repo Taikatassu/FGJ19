a1245cf81a976ccd94f1fe1c3ce17c1f33b1c21a
Turboherne <turbohernemail@gmail.com>
2019-01-26 15:00:31 +0200

    Player prefab almost finished

---
+++ FGJ19/Assets/Code/Player.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    Transform targetDirection;
+    Transform travelPos;
+    public GameObject looker;
+    public GameObject rotator;
+    public GameObject reticle;
+    bool raycastSuccessful = false;
+    bool hookLerpOK = false;
+    bool hookLerpReturn = false;
+    Transform hook;
+    float currentHookLerpTime;
+    public float hookLerpTime;
+
+    void Start()
+    {
+        targetDirection = new GameObject("targetDirection").transform;
+        travelPos = new GameObject("travelPos").transform;
+        hook = new GameObject("hook").transform;
+    }
+
+
+    void Update()
+    {
+        if (!hookLerpOK && !hookLerpReturn)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                RaycastHit2D hitInfo = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
+                Debug.DrawRay(Camera.main.ScreenPointToRay(Input.mousePosition).origin, Camera.main.ScreenPointToRay(Input.mousePosition).direction, Color.red, 5f);
+                print("raycasted");
+                if (hitInfo.collider != null)
+                {
+                    raycastSuccessful = true;
+                    targetDirection.position = hitInfo.point;
+                    print("raycast position: " + hitInfo.point);
+
+                    //Make looker look at input position
+                    //looker.transform.LookAt(targetDirection);
+                    var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(looker.transform.position);
+                    var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                    looker.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+                    //Enable looker
+                    looker.SetActive(true);
+                }
+                else
+                {
+                    print("raycast failed!");
+                    raycastSuccessful = false;
+                }
+            }
+            if (Input.GetMouseButtonUp(0) && raycastSuccessful)
+            {
+                looker.SetActive(false);
+                travelPos.position = reticle.transform.position;
+                currentHookLerpTime = 0;
+                hookLerpOK = true;
+            }
+        }
+
+        if (hookLerpOK)
+        {
+            //increment timer once per frame
+            currentHookLerpTime += Time.deltaTime;
+            if (currentHookLerpTime > hookLerpTime)
+            {
+                currentHookLerpTime = hookLerpTime;
+            }
+
+            //lerp!
+            float perc = currentHookLerpTime / hookLerpTime;
+            hook.position = Vector3.Lerp(transform.position, travelPos.position, perc);
+            print("currenthooklerptime = " + currentHookLerpTime);
+            if (currentHookLerpTime == hookLerpTime)
+            {
+                hookLerpOK = false;
+                hookLerpReturn = true;
+                currentHookLerpTime = 0;
+            }
+        }
+        if (hookLerpReturn)
+        {
+            //increment timer once per frame
+            currentHookLerpTime += Time.deltaTime;
+            if (currentHookLerpTime > hookLerpTime)
+            {
+                currentHookLerpTime = hookLerpTime;
+            }
+
+            //lerp!
+            float perc = currentHookLerpTime / hookLerpTime;
+            hook.position = Vector3.Lerp(travelPos.position, transform.position, perc);
+            print("currenthooklerptime = " + currentHookLerpTime);
+            if (currentHookLerpTime == hookLerpTime)
+            {
+                hookLerpReturn = false;
+            }
+        }
+    }
+}
---
+++ FGJ19/Assets/Code/Rotator.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotationAngle = 90;
+    float trueRotAngle;
+    float currentLerpTime;
+    public float lerpTime = 1;
+    bool rotateRight = false;
+    bool rotateLeft = false;
+    void Start()
+    {
+        trueRotAngle = rotationAngle / 2;
+        transform.localEulerAngles = new Vector3(0, 0, trueRotAngle);
+        rotateRight = true;
+    }
+
+
+    void Update()
+    {
+        if (rotateRight)
+        {
+            //increment timer once per frame
+            currentLerpTime += Time.deltaTime;
+            if (currentLerpTime > lerpTime)
+            {
+                currentLerpTime = lerpTime;
+            }
+
+            //lerp!
+            float perc = currentLerpTime / lerpTime;
+            float angle = Mathf.LerpAngle(trueRotAngle, -trueRotAngle, perc);
+            print(angle);
+            transform.localEulerAngles = new Vector3(0, 0, angle);
+            print("currenthooklerptime = " + currentLerpTime);
+            if (currentLerpTime == lerpTime)
+            {
+                print("turning left");
+                rotateRight = false;
+                rotateLeft = true;
+                currentLerpTime = 0;
+            }
+        }
+        if (rotateLeft)
+        {
+            //increment timer once per frame
+            currentLerpTime += Time.deltaTime;
+            if (currentLerpTime > lerpTime)
+            {
+                currentLerpTime = lerpTime;
+            }
+
+            //lerp!
+            float perc = currentLerpTime / lerpTime;
+            float angle = Mathf.LerpAngle(-trueRotAngle, trueRotAngle, perc);
+            print(angle);
+            transform.localEulerAngles = new Vector3(0, 0, angle);
+            print("currenthooklerptime = " + currentLerpTime);
+            if (currentLerpTime == lerpTime)
+            {
+                print("turning right");
+                rotateLeft = false;
+                rotateRight = true;
+                currentLerpTime = 0;
+            }
+        }
+    }
+}
