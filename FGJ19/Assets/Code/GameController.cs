d86b547c6798c253a97e6895ab736e57053a5a30
Turboherne <turbohernemail@gmail.com>
2019-01-26 18:56:20 +0200

    Made some stuff for UI, waiting for scene

---
+++ FGJ19/Assets/Code/GameController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+
+    public Slider timerBar;
+    public GameObject gameOverUI;
+    public GameObject disableControls;
+    float decreasableAmount = 0.16f;
+    bool gameOvered = false;
+
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (timerBar.value > 0)
+        {
+            timerBar.value -= decreasableAmount * Time.deltaTime;
+        }
+        else if (!gameOvered)
+        {
+            //DISABLE CONTROLS!!
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        gameOvered = true;
+        disableControls.SetActive(true);
+        gameOverUI.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Scene00");
+    }
+
+    public void KeepGoing()
+    {
+        gameOverUI.SetActive(false);
+        disableControls.SetActive(false);
+        timerBar.value = 1;
+        gameOvered = false;
+    }
+}
